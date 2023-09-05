6bd7c367d21d77c7d6540bfbf788f6d68acc90f7
Jonathan <sccp.jolopez@gmail.com>
2023-09-05 13:04:41 -0500

    prueba N3

---
+++ app.js
@@ -0,0 +1,16 @@
+let botonElem = document.getElementById('boton-cambiar-cita');
+let citaElem  = document.getElementById('cita');
+let autorElem = document.getElementById('autor');
+
+function generarAleatorio(min, max){
+    // Sin incluir max en los valores posibles
+    return Math.floor(Math.random()*(max-min)+min);
+}
+function cambiarCita(){
+    let indiceAleatorio = generarAleatorio(0,citas.length);
+    citaElem.innerText  = `"${citas[indiceAleatorio].texto}"`;
+    autorElem.innerText = citas[indiceAleatorio].autor;
+}
+
+cambiarCita();
+botonElem.addEventListener('click', cambiarCita)
---
+++ citas.js
@@ -0,0 +1,86 @@
+let citas = [
+    {
+      'autor': 'Albert Einstein',
+      'texto': 'No poseo talentos especiales. Solo soy apasionadamente curioso.'
+    },
+    {
+      'autor': 'Anónimo',
+      'texto': 'Semanas de programación te pueden ahorrar horas de planeación.'
+    },
+    {
+      'autor': 'Alan Kay',
+      'texto': 'La mejor forma de predecir el futuro es inventarlo.'
+    },
+    {
+      'autor': 'Amelia Earhart',
+      'texto': 'Lo más dificil es tomar la decisión de actuar. El resto es simplemente tenacidad.'
+    },
+    {
+      'autor': 'Aristotle',
+      'texto': 'La calidad no es un acto, es un hábito.'
+    },
+    {
+      'autor': 'Benjamin Franklin',
+      'texto': 'Dímelo y lo olvidaré. Enséñamelo y lo recordaré. Involúcrame y lo aprenderé.'
+    },
+    {
+      'autor': 'Charles R. Swindoll',
+      'texto': 'La vida es 10% lo que te ocurre y 90% cómo reaccionas.'
+    },
+    {
+      'autor': 'Jane Goodall',
+      'texto': 'Lo que haces hace una diferencia. Y debes decidir si qué tipo de diferencia quieres hacer.'
+    },
+    {
+      'autor': 'John Muir',
+      'texto': 'El poder de la imaginación nos hace infinitos.'
+    },
+    {
+      'autor': 'Mark Twain',
+      'texto': 'Los dos días más importantes de tu vida son el día que naciste y el día que descubres por qué.'
+    },
+    {
+      'autor': 'Steve Jobs',
+      'texto': 'El único modo de hacer un gran trabajo es amar lo que haces.'
+    },
+    {
+      'autor': 'Michael Jordan',
+      'texto': 'Nunca pienso en las consecuencias de fallar un gran tiro… cuando se piensa en las consecuencias se está pensando en un resultado negativo.'
+    },
+    {
+      'autor': 'Nelson Mandela',
+      'texto': 'El dinero no es la clave del éxito; la libertad para poder crear lo es.'
+    },
+    {
+      'autor': 'Gary Player',
+      'texto': 'Cuanto más duramente trabajo, más suerte tengo.'
+    },
+    {
+      'autor': 'Aristóteles',
+      'texto': 'La inteligencia consiste no sólo en el conocimiento, sino también en la destreza de aplicar los conocimientos en la práctica.'
+    },
+    {
+      'autor': 'Helena Rubinstein',
+      'texto': 'El trabajo duro hace que desaparezcan las arrugas de la mente y el espíritu.'
+    },
+    {
+      'autor': 'Elon Musk',
+      'texto': 'Cuando algo es lo suficientemente importante, lo haces incluso si las probabilidades de que salga bien no te acompañan.'
+    },
+    {
+      'autor': 'Confucio',
+      'texto': 'Escoge un trabajo que te guste, y nunca tendrás que trabajar ni un solo día de tu vida.'
+    },
+    {
+      'autor': 'Colin Powell',
+      'texto': 'Un sueño no se hace realidad por arte de magia, necesita sudor, determinación y trabajo duro .'
+    },
+    {
+      'autor': 'Benjamin Franklin ',
+      'texto': 'Cuéntamelo y me olvidaré. enséñamelo y lo recordaré. involúcrame y lo aprenderé .'
+    },
+    {
+      'autor': 'Albert Einstein ',
+      'texto': 'La lógica te llevará de la a a la z. la imaginación te llevará a cualquier lugar.'
+    },
+  ];
